9a6e6826f32174b6047dbb5f56e19818b7423c9a
unknown <alvesp2001@gmail.com>
2020-11-03 16:43:45 -0300

    migrations

---
+++ app/database/migrations/20201030182741-create-users.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    
+      return queryInterface.createTable('users', { 
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        phone_number: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },        
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
